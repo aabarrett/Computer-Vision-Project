d40144f54766ce037599c90352a85919ab4e5922
Amy Barrett <aab357@mun.ca>
2020-03-14 20:40:37 -0230

    ye

--- main.py
+++ main.py
@@ -7,6 +7,11 @@ from tensorflow.keras import optimizers
 from tensorflow.keras.layers import Dense, Conv2D, Flatten, Dropout, MaxPooling2D, BatchNormalization, \
     GlobalAveragePooling2D
 from tensorflow.keras.preprocessing.image import ImageDataGenerator
+from matplotlib import pyplot
+from matplotlib.image import imread
+import os
+import numpy as np
+import pickle
 
 os.chdir('stanford-dogs-dataset/images/Images')
 d = '.'
@@ -16,7 +21,7 @@ training_names = [os.path.join(d, o) for o in os.listdir(d)
 images = []
 names = []
 path = os.getcwd()
-image_size = 32
+image_size = 128
 
 for image_folder in training_names:
     for file_name in os.listdir(image_folder):
@@ -27,23 +32,21 @@ for image_folder in training_names:
             images.append(cv2.resize(image, (image_size, image_size)))
             names.append(image_folder)
 
-train_x, test_x, train_y, test_y = train_test_split(images, names, test_size=0.2)
+y_train_raw = np.array(names, np.uint8)
+x_train_raw = np.array(images, np.float32) / 255.
+num_class = 120 #y_train_raw.shape[1]
+
+train_x, test_x, train_y, test_y = train_test_split(x_train_raw, y_train_raw, test_size=0.2)
 
 # plot dog photos from the dogs vs cats dataset
 
-
-from matplotlib import pyplot
-from matplotlib.image import imread
-import os
-import numpy as np
 batch_size = 128
 epochs = 20
 learning_rate = 0.001
 
-num_class = y_train_raw.shape[1]
 
 model = Sequential()
-model.add(BatchNormalization(input_shape=(128, 128, 3)))
+model.add(BatchNormalization(input_shape=(image_size, image_size, 3)))
 model.add(Conv2D(filters=16, kernel_size=3, kernel_initializer='he_normal', activation='relu'))
 model.add(MaxPooling2D(pool_size=2))
 model.add(BatchNormalization())
@@ -65,7 +68,15 @@ model.summary()
 
 # opt = optimizers.Adam(lr = learning_rate)
 model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
-# history = model.fit(x_train, y_train, validation_data=(x_test, y_test), batch_size=batch_size,verbose=2, epochs=epochs, shuffle = True)
+history = model.fit(train_x, train_y, validation_data=(test_x, test_y), batch_size=batch_size,verbose=2, epochs=epochs, shuffle = True)
 
-# https://machinelearningmastery.com/how-to-develop-a-convolutional-neural-network-to-classify-photos-of-dogs-and-cats/
-# https://medium.com/@claymason313/dog-breed-image-classification-1ef7dc1b1967
+# save the model to disk
+filename = 'finalized_model.sav'
+pickle.dump(model, open(filename, 'wb'))
+
+# some time later...
+
+# load the model from disk
+#loaded_model = pickle.load(open(filename, 'rb'))
+#result = loaded_model.score(X_test, Y_test)
+#print(result)
