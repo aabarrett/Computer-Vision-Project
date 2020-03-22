a7092945e1e0fcaa00dedb8764bf017e083475fb
Neilerino <naw218@mun.ca>
2020-03-22 19:03:32 -0230

    possible working training

--- main.py
+++ main.py
@@ -7,9 +7,11 @@ from tensorflow.keras.models import Sequential, Model
 from tensorflow.keras.callbacks import ModelCheckpoint
 from tensorflow.keras import optimizers
 from tensorflow.keras.layers import Dense, Conv2D, Flatten, Dropout, MaxPooling2D, BatchNormalization, \
-    GlobalAveragePooling2D
-from tensorflow.keras.preprocessing.image import ImageDataGenerator
+    GlobalAveragePooling2D, UpSampling2D
+# from tensorflow.keras.preprocessing.image import imgDataGenerator
+from tensorflow.keras.preprocessing import image
 from tensorflow.keras.applications import resnet50
+from tensorflow.keras.applications.resnet50 import preprocess_input, decode_predictions
 from matplotlib import pyplot
 from matplotlib.image import imread
 import os
@@ -39,105 +41,73 @@ def matrix_Bin(labels):
     
     return labels_name, labels_bin
 
+# Setup Model
+img_size = 128
+
+resnet = resnet50.ResNet50(include_top=False, weights='imagenet', input_shape=(img_size, img_size, 3))
+
+for layer in resnet.layers:
+    if isinstance(layer, BatchNormalization):
+        layer.trainable = True
+    else:
+        layer.trainable = False
+
+model = Sequential()
+model.add(resnet)
+model.add(GlobalAveragePooling2D())
+model.add(Dense(256, activation='relu'))
+# model.add(Dropout(0.5))
+model.add(BatchNormalization())
+model.add(Dense(120, activation='softmax'))
+
+model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
+
 os.chdir('stanford-dogs-dataset/images/Images')
 d = '.'
 training_names = [os.path.join(d, o) for o in os.listdir(d)
                   if os.path.isdir(os.path.join(d, o))]
 
-image_size = 128
-images = []
+imgs = []
 names = []
 path = os.getcwd()
 print('Parsing Files for dataset')
-for image_folder in training_names:
-    for file_name in os.listdir(image_folder):
+for img_folder in training_names:
+    for file_name in os.listdir(img_folder):
         if 'n02' in file_name :
-            image = cv2.imread(path + '/' + image_folder[2:] + '/' + file_name)
-            if image is None:
+            img = image.load_img(path + '/' + img_folder[2:] + '/' + file_name, target_size=(img_size, img_size))
+            # img = cv2.imread(path + '/' + img_folder[2:] + '/' + file_name)
+            if img is None:
                 print(file_name)
-            images.append(cv2.resize(image, (image_size, image_size)))
-            names.append(image_folder)
+            img = image.img_to_array(img)
+            img = np.expand_dims(img, axis=0)
+            img = preprocess_input(img)
+            imgs.append(img[0])
+            # imgs.append(cv2.resize(img, (img_size, img_size)))
+            names.append(img_folder)
 
-# y_train_raw = np.array(names, np.uint8)
-x_train_raw = np.array(images, np.float32) / 255.
-num_class = 120 #y_train_raw.shape[1]
+# x_train_raw = np.array(imgs, np.float32) / 255
+x_train_raw = np.array(imgs, np.float32)
+
+num_class = 120
 
 labels_name, labels_bin = matrix_Bin(names)
 
 train_x, test_x, train_y, test_y = train_test_split(x_train_raw, labels_bin, test_size=0.2)
 
+#Pre-process the data
+# train_x = preprocess_input(train_x)
+# test_x = preprocess_input(test_x)
+
 batch_size = 128
 epochs = 20
 learning_rate = 0.001
 
-# Setup the resnet model for transfer learning
-# resnet = resnet50.ResNet50(include_top=False, weights='imagenet', input_shape=(image_size, image_size, 3))
-# resnet.summary()
-
-# resnet.trainable = False
-
-# model = Sequential()
-# model.add(resnet)
-# model.add(Flatten())
-# model.add(Dense(256, activation='relu'))
-# model.add(Dense(1, activation='sigmoid'))
-
-# model.add(Conv2D(32, (3, 3), activation='relu', input_shape=(image_size, image_size, 3)))
-# model.add(MaxPooling2D((2,2)))
-# model.add(Conv2D(64, (3, 3), activation='relu'))
-# model.add(MaxPooling2D((2,2)))
-# model.add(Conv2D(128, (3, 3), activation='relu'))
-# model.add(MaxPooling2D((2,2)))
-# model.add(Conv2D(128, (3, 3), activation='relu'))
-# model.add(MaxPooling2D((2,2)))
-# model.add(Flatten())
-# model.add(Dropout(0.5))
-# model.add(Dense(512, activation='relu'))
-# model.add(Dense(1, activation='sigmoid'))
-
-
-###### ============================
-# Setup Model
-
-resnet = resnet50.ResNet50(include_top=False, weights='imagenet', input_shape=(image_size, image_size, 3))
-resnet.summary()
-
-resnet.trainable = False
-
-model = Sequential()
-model.add(resnet)
-model.add(Flatten())
 model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
-model.add(BatchNormalization(input_shape=(image_size, image_size, 3)))
-model.add(Conv2D(filters=16, kernel_size=3, kernel_initializer='he_normal', activation='relu'))
-model.add(MaxPooling2D(pool_size=2))
-model.add(BatchNormalization())
-model.add(Conv2D(filters=32, kernel_size=3, kernel_initializer='he_normal', activation='relu'))
-model.add(MaxPooling2D(pool_size=2))
-model.add(BatchNormalization())
-model.add(Conv2D(filters=64, kernel_size=3, kernel_initializer='he_normal', activation='relu'))
-model.add(MaxPooling2D(pool_size=2))
-model.add(BatchNormalization())
-model.add(Conv2D(filters=128, kernel_size=3, kernel_initializer='he_normal', activation='relu'))
-model.add(MaxPooling2D(pool_size=2))
-model.add(BatchNormalization())
-model.add(Conv2D(filters=256, kernel_size=3, kernel_initializer='he_normal', activation='relu'))
-model.add(MaxPooling2D(pool_size=2))
-model.add(BatchNormalization())
-model.add(GlobalAveragePooling2D())
-model.add(Dense(num_class, activation='softmax'))
-
-model.summary()
-
-# # opt = optimizers.Adam(lr = learning_rate)
-# model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
 
 two_up = os.path.abspath(os.path.join(__file__ ,"../.."))
 os.chdir(two_up)
 
-# checkpoint = ModelCheckpoint(filepath='checkpoints.hdf5', verbose=1, save_best_only=True)
-
-history = model.fit(train_x, train_y, validation_data=(test_x, test_y), batch_size=batch_size, verbose=2, epochs=epochs, shuffle = True)
+history = model.fit(train_x, train_y, batch_size=batch_size, validation_data=(test_x, test_y), verbose=2, epochs=epochs, shuffle = True)
 
 model.save('saved_model/my_model.h5')
 
